07520dfb59f080ae7b889b3fb894400557d7f2d5
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2023-01-12 20:46:01 -0500

    update data for app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <div id="app">
-    <h1>Hola inicial vuex</h1>
+  <div class="wrapper">
+    <div v-for="airport in airports" :key="airport.abbreviation">
+      <airport-card :airport="airport" />
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import allAirports from '@/data/airports.js'
+import AirportCard from '@/components/AirportCard.vue'
 
 export default {
-  name: 'App',
+  components: {
+    AirportCard
+  },
+  setup() {
+    const airports = ref(allAirports)
+    return { airports }
+  }
 }
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 1rem;
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+p,
+h3 {
+  grid-column: span 3;
+}
 </style>
